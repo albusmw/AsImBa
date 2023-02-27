9201ab5220702ea85c651646c180b32bac466ee3
albusmw <albusmw@gmx.de>
2023-02-27 10:33:39 +0100

    function moved

--- Helpers/Functions.vb
+++ Helpers/Functions.vb
@@ -3,25 +3,6 @@ Option Strict On
 
 Public Class Functions
 
-    '''<summary>Sort the passed dictionary according to T1 (key).</summary>
-    Public Shared Function SortDictionary(Of T1, T2)(ByRef Hist As Dictionary(Of T1, T2)) As Dictionary(Of T1, T2)
-
-        'Generate a list
-        Dim KeyList As New List(Of T1)
-        For Each Entry As T1 In Hist.Keys
-            KeyList.Add(Entry)
-        Next Entry
-        'Sort keys
-        KeyList.Sort()
-        'Re-generate dictionary
-        Dim RetVal As New Dictionary(Of T1, T2)
-        For Each Entry As T1 In KeyList
-            RetVal.Add(Entry, Hist(Entry))
-        Next Entry
-        Return RetVal
-
-    End Function
-
     '''<summary>Convert the passed (sorted or not) dictionary to 2 vectors, e.g. for plot purpose.</summary>
     Public Shared Sub HistoToXY(ByRef Dict As Dictionary(Of Double, Integer), ByRef X As Double(), ByRef Y As Double())
 
